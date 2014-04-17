38ecc6b0e67a8c056b05177582c173d9a8d92eac
ezdon <ezdon123@gmail.com>
2014-04-17 13:24:59 +0100

    Revert "uhiuh"
    This reverts commit e9b010deba2483c35273af99cb3b886108d3c883.

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -4,7 +4,7 @@
 # developed in the Python 3.2 programming environment
 # version 2 edited 06/03/2014
 import random
-import tabulate
+
 NO_OF_RECENT_SCORES = 3
 
 class TCard():
@@ -178,8 +178,7 @@ def DisplayRecentScores(RecentScores):
   print('Recent Scores: ')
   print()
   for Count in range(1, NO_OF_RECENT_SCORES + 1):
-    table = [RecentScores[Count].Name,RecentScores[Count].Score]
-  print tabulate(table, headers=['Name','Score'])
+    print(RecentScores[Count].Name, 'got a score of', RecentScores[Count].Score)
   print()
   print('Press the Enter key to return to the main menu')
   input()
